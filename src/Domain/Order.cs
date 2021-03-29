27843c84826c6280ae0ff16e1f7f36067428c835
Gavin van der Merwe <gavin.vandermerwe@smarsh.com>
2021-03-29 22:09:38 +0100

    Adding domain object Order with failing test

---
+++ src/Domain/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain
+{
+    public class Order
+    {
+        /* Should include user profile but does not */
+        
+        public Order(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Product)}: {Product}, {nameof(Quantity)}: {Quantity}";
+        }
+    }
+}
